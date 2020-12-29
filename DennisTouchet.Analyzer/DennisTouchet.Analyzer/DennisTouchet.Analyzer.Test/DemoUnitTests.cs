74e2665c9eaee32542666c6e09932a7cc2c72bd5
Trey Touchet <trey.touchet@mindbodyonline.com>
2020-12-28 21:26:05 -0800

    add simple demo analyzer

---
+++ DennisTouchet.Analyzer/DennisTouchet.Analyzer/DennisTouchet.Analyzer.Test/DemoUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DennisTouchet.Analyzer.Test
+{
+    class DemoUnitTests
+    {
+        public void Sample()
+        {
+            string.Equals("", "");
+        }
+    }
+}
---
+++ DennisTouchet.Analyzer/DennisTouchet.Analyzer/DennisTouchet.Analyzer/Constants/RuleCategories.cs
@@ -0,0 +1,9 @@
+﻿namespace DennisTouchet.Analyzer.Constants
+{
+    internal static class RuleCategories
+    {
+        public const string Naming = "Naming";
+        public const string Style = "Style";
+        public const string Usage = "Usage";
+    }
+}
